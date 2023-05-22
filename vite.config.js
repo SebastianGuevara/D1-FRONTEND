c0ef6300dd4eda1153bc8be2e11191c14970ceee
SebastianGuevara <sebastianguve@unisabana.edu.co>
2023-05-21 22:31:08 -0500

    Corrige vite build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    commonjsOptions: { include: [] },
+  },
+  optimizeDeps: {
+    disabled: false,
+  },
+  server: {
+    host: true,
+    strictPort: true,
+  }
 })
